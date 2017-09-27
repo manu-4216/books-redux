a42ee9dec9730633fb38077030c3a7d4cc5cce99
Manuel Micu <manu4216@gmail.com>
2017-09-27 06:39:04 +0200

    Add redux reducer and connect for books

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        { title: 'Harry Potter' },
+        { title: 'Javascript: The good parts' },
+        { title: 'War and peace' },
+        { title: 'On intelligence' }
+    ];
+}
